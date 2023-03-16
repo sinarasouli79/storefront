b20b2148dfde129a519648e2c32382f52d4af703
sinarasouli79 <sinarasouli79@gmail.com>
2023-03-16 07:27:36 +0330

    limiting results

--- playground/views.py
+++ playground/views.py
@@ -5,16 +5,6 @@ from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.order_by('title')
-    queryset = Product.objects.order_by('-title')
-    queryset = Product.objects.filter(unit_price__gte=20).order_by('-title')
-    queryset = Product.objects.filter(unit_price__gte=20).order_by('-title').reverse()
-    print('earliest')
-    print(Product.objects.filter(unit_price__gte=20).earliest('-title'))
-    print(Product.objects.filter(unit_price__gte=20).order_by('-title')[0])
-
-    print('latest')
-    print(Product.objects.filter(unit_price__gte=20).latest('-title'))
-    # print(Product.objects.filter(unit_price__gte=20).order_by('-title')[-1]) #negetive indexing is not supported.
-
+    queryset = Product.objects.all()[:5]
+    queryset = Product.objects.all()[5:10]
     return render(request, 'hello.html', {'name': 'Mosh', 'object_list': queryset})
