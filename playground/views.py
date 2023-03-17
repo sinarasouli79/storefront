67377d44d18b7324b9761b2c752524d1b6a82e12
sinarasouli79 <sinarasouli79@gmail.com>
2023-03-17 19:48:16 +0330

    creating objects

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product
-from tags.models import TaggedItem
+from store.models import Collection, Product
 
 
 def say_hello(request):
 
-    queryset = Product.objects.all()
-    list(queryset)
-    list(queryset)
-    queryset[0]
-    return render(request, 'hello.html', {'name': 'Mosh', 'result': queryset})
+    collection = Collection()#title='test collectoin', featured_product_id=1
+    collection.title = 'test collectoin'
+    collection.featured_product = Product(pk=1)
+    collection.featured_product_id = 1
+    collection.save()
+    # collection = Collection.objects.create(title='test collectoin', featured_product_id=1)
+    return render(request, 'hello.html', {'name': 'Mosh'})
