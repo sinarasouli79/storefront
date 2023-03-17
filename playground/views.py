14ea7b383fb501de021896336055135e333a8cb0
sinarasouli79 <sinarasouli79@gmail.com>
2023-03-17 18:39:47 +0330

    working with expression wrappers exercise pdf

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
-from django.db.models import ExpressionWrapper, F, DecimalField
-from store.models import Product
+from django.db.models import ExpressionWrapper, F, DecimalField, Max, Count, Sum
+from store.models import Customer, Collection, Product, Order
 
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(discounted_price=discounted_price)
 
+    # 1
+    queryset = Customer.objects.annotate(last_order_id=Max('order'))
+
+    # 2
+    queryset = Collection.objects.annotate(product_count=Count('product'))
+
+    # 3
+    queryset = Customer.objects.annotate(
+        order_count=Count('order')).filter(order_count__gt=5)
+
+    #4
+    queryset = Customer.objects.annotate(
+        total_spent=Sum(
+        F('order__orderitem__quantity')* F('order__orderitem__unit_price')
+        )
+    )
+
+    # 5
+    queryset = Product.objects.annotate(
+        total_sale=Sum(F('orderitem__quantity') * F('orderitem__unit_price'))
+        # total_sale=F('orderitem__quantity') * F('orderitem__unit_price')
+        ).order_by('-total_sale')[:5]
+
+    print(queryset)
     return render(request, 'hello.html', {'name': 'Mosh', 'result': queryset})
