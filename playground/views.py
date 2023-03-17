f712609e2dd455e6e868801d7d75e1c4a360345a
sinarasouli79 <sinarasouli79@gmail.com>
2023-03-17 18:56:11 +0330

    quering generic relationships

--- playground/views.py
+++ playground/views.py
@@ -1,32 +1,17 @@
 from django.shortcuts import render
-from django.db.models import ExpressionWrapper, F, DecimalField, Max, Count, Sum
-from store.models import Customer, Collection, Product, Order
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
 
-    # 1
-    queryset = Customer.objects.annotate(last_order_id=Max('order'))
-
-    # 2
-    queryset = Collection.objects.annotate(product_count=Count('product'))
-
-    # 3
-    queryset = Customer.objects.annotate(
-        order_count=Count('order')).filter(order_count__gt=5)
-
-    #4
-    queryset = Customer.objects.annotate(
-        total_spent=Sum(
-        F('order__orderitem__quantity')* F('order__orderitem__unit_price')
+    content_type = ContentType.objects.get_for_model(Product)
+    print(content_type)
+    queryset = TaggedItem.objects\
+        .select_related('tag')\
+        .filter(
+            content_type=content_type,
+            object_id=1
         )
-    )
-
-    # 5
-    queryset = Product.objects.annotate(
-        total_sale=Sum(F('orderitem__quantity') * F('orderitem__unit_price'))
-        # total_sale=F('orderitem__quantity') * F('orderitem__unit_price')
-        ).order_by('-total_sale')[:5]
-
-    print(queryset)
     return render(request, 'hello.html', {'name': 'Mosh', 'result': queryset})
