4b37e13cc7168cb463191a089310e1929d872715
sinarasouli79 <sinarasouli79@gmail.com>
2023-03-17 10:06:07 +0330

    aggregating objects(exercise pdf)

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.db.models.aggregates import Count, Min
-from store.models import Product
+from django.db.models.aggregates import Count, Sum, Min, Max, Avg
+from django.db.models import F
+from store.models import Order, OrderItem, Product
+
 
 def say_hello(request):
 
-    result = Product.objects.aggregate(min=Min('unit_price'), count=Count('id'))
+    # 1
+    result = Order.objects.aggregate(count=Count(id))
+
+    # 2
+    result = OrderItem.objects.filter(
+        product__id=1).aggregate(units_sold=Sum('quantity'))
+    # print(result)
+    result = Product.objects.filter(id=1).aggregate(
+        units_sold=Sum('orderitem__quantity'))
+
+    # 3
+    result = Order.objects.filter(customer__id=1).aggregate(Count('id'))
+
+    # 4
+    result = Product.objects \
+        .filter(collection__id=3) \
+        .aggregate(
+            min_price=Min('unit_price'),
+            max_price=Max('unit_price'),
+            avg_price=Avg('unit_price')
+        )
     return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
