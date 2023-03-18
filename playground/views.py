88ce95d841edab04bae5dde10bbb351b12c5b811
sinarasouli79 <sinarasouli79@gmail.com>
2023-03-18 09:35:13 +0330

    executing raw sequel queries

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
-from store.models import Order, OrderItem
-from django.db import transaction
+from store.models import Product
+from django.db import connection
 
 
-# @transaction.atomic
 def say_hello(request):
-    with transaction.atomic:
-        order = Order()
-        order.customer_id = 10
-        order.save()
 
-        item = OrderItem()
-        item.order = order
-        item.product_id = 1
-        item.quantity = -4
-        item.unit_price = 23.00
-        item.save()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # queryset = Product.objects.raw('SELECT * FROM store_product')
+
+    # cursor = connection.cursor()
+    # queryset = cursor.execute('SELECT * FROM store_product')
+    # cursor.close()
+
+    with connection.cursor() as cursor:
+        # queryset =cursor.execute('SELECT * FROM store_product')
+        queryset =cursor.callproc('get_customers', [1, 2, 'a'])
+        
+
+    return render(request, 'hello.html', {'name': 'Mosh', 'result':queryset})
