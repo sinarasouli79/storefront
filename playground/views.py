4055c40a7ab6fe14281dd80df6c52fc84224b18a
sinarasouli79 <sinarasouli79@gmail.com>
2023-03-18 07:02:36 +0330

    deleting objects exercise pdf

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from store.models import Collection, Product
+from store.models import Collection, Cart, CartItem
 
 
 def say_hello(request):
+    # 1
+    # cart = Cart.objects.create()
+    # item = CartItem()
+    # item.cart_id = 1
+    # item.product_id = 1
+    # item.quantity = 3
+    # item.save()
 
-    collection = Collection(pk=11)
-    collection.delete()
+    # 2
+    # item = CartItem.objects.get(pk=1)
+    # item.quantity = 6
+    # item.save()
 
-    Collection.objects.filter(id__gt=5).delete()
+    # 3
+    # Cart.objects.get(id=1).delete()
+    # cart=Cart(pk=1)
+    # cart.delete()
     return render(request, 'hello.html', {'name': 'Mosh'})
