bb0628e2e1cb7511efffde8b9ea110ec6b7e8f54
sinarasouli79 <sinarasouli79@gmail.com>
2023-03-18 07:28:34 +0330

    transactions

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render
-from store.models import Collection, Cart, CartItem
+from store.models import Order, OrderItem
+from django.db import transaction
 
 
+# @transaction.atomic
 def say_hello(request):
-    # 1
-    # cart = Cart.objects.create()
-    # item = CartItem()
-    # item.cart_id = 1
-    # item.product_id = 1
-    # item.quantity = 3
-    # item.save()
+    with transaction.atomic:
+        order = Order()
+        order.customer_id = 10
+        order.save()
 
-    # 2
-    # item = CartItem.objects.get(pk=1)
-    # item.quantity = 6
-    # item.save()
-
-    # 3
-    # Cart.objects.get(id=1).delete()
-    # cart=Cart(pk=1)
-    # cart.delete()
+        item = OrderItem()
+        item.order = order
+        item.product_id = 1
+        item.quantity = -4
+        item.unit_price = 23.00
+        item.save()
     return render(request, 'hello.html', {'name': 'Mosh'})
