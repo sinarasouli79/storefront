1957b347987a1d44319604c3f2a61d496a377fbb
sinarasouli79 <sinarasouli79@gmail.com>
2023-04-12 19:27:19 +0330

    providing links to other pages

--- store/admin.py
+++ store/admin.py
@@ -1,27 +1,41 @@
 from django.contrib import admin
 from . import models
 from django.db.models import Count
+from django.utils.html import format_html
+from django.utils.http import urlencode
+from django.urls import reverse
 # Register your models here.
 
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
 
+    @admin.display(ordering='orders_count')
+    def orders_count(self, customer):
+        url = (reverse('admin:store_order_changelist')
+               + '?'
+               + urlencode({'customer__id':customer.id}))
+        return format_html(f"<a href={url}>{customer.orders_count}</a>")
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(orders_count=Count('order'))
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
     list_editable = ['unit_price']
     ordering = ['title', 'unit_price']
     list_per_page = 10
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
-        return 'Low' if product.inventory else 'Ok'
+        return 'Low' if product.inventory < 10 else 'Ok'
+
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
@@ -31,8 +45,10 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
-        return collection.products_count
-    
+        url = (reverse('admin:store_product_changelist')
+               + '?'
+               + urlencode({'collection__id': collection.id}))
+        return format_html(f"<a href={url}>{collection.products_count}</a>")
 
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(products_count=Count('product'))
@@ -43,9 +59,11 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['payment_status', 'placed_at', 'customer']
     list_per_page = 10
 
+
 @admin.register(models.OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ['order', 'product', 'product_description', 'quantity', 'unit_price']
+    list_display = ['order', 'product',
+                    'product_description', 'quantity', 'unit_price']
     list_select_related = ['order', 'product']
     list_per_page = 10
 
--- store/models.py
+++ store/models.py
@@ -25,6 +25,8 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
 
 class Product(models.Model):
     objects = TaggedItemManager()
