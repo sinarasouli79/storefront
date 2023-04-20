af1e16d03a11dcead4d434a65626baa2787ba2ff
sinarasouli79 <sinarasouli79@gmail.com>
2023-04-20 09:55:22 +0330

    creating custom action

--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
-from . import models
+from django.contrib import admin, messages
 from django.db.models import Count
+from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.http import urlencode
-from django.urls import reverse
+
+from . import models
+
 # Register your models here.
 
 
@@ -19,40 +21,45 @@ class CustomerAdmin(admin.ModelAdmin):
     def orders_count(self, customer):
         url = (reverse('admin:store_order_changelist')
                + '?'
-               + urlencode({'customer__id':customer.id}))
+               + urlencode({'customer__id': customer.id}))
         return format_html(f"<a href={url}>{customer.orders_count}</a>")
-    
+
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(orders_count=Count('order'))
-
 
 
 class inventory_status(admin.SimpleListFilter):
     title = 'inventory'
     parameter_name = 'inventory'
     less_than_ten = '<10'
+
     def lookups(self, request, model_admin):
         return [(self.less_than_ten, 'low')]
-    
+
     def queryset(self, request, queryset):
         if self.value() == self.less_than_ten:
             return queryset.filter(inventory__lt=10)
-    
-
-
 
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory', 'inventory_status', 'collection']
+    actions = ['clear_inventory']
+    list_display = ['id', 'title', 'unit_price',
+                    'inventory_status', 'collection', 'last_update']
     list_editable = ['unit_price']
-    list_filter = ['collection', inventory_status]
+    list_filter = ['collection', 'last_update', inventory_status]
     list_per_page = 10
     ordering = ['title', 'unit_price']
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
-        return 'Low' if product.inventory < 10 else 'Ok'
+        return f'{product.inventory} (Low)' if product.inventory < 10 else f'{product.inventory} (Ok)'
+
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        update_count = queryset.update(inventory=0)
+        self.message_user(request, f'{update_count} product(s) sucessfully updated.',
+                          messages.ERROR)
 
 
 @admin.register(models.Collection)
