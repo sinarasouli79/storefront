70d072c6a82bbd5c48bba2436e21506d5af8314f
sinarasouli79 <sinarasouli79@gmail.com>
2023-04-27 19:01:01 +0330

    viewsetes

--- store/views.py
+++ store/views.py
@@ -1,24 +1,16 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Collection, Product
 from .serializers import CollectionSerializer, ProductSerializer
 
+
 # Create your views here.
-
-
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
-    serializer_class = ProductSerializer
-
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+    
     def perform_destroy(self, instance):
         if instance.orderitem_set.exists():
             return Response({'error': 'There are some order items associated with this product that you should delete first.'},
@@ -26,13 +18,8 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         return super().perform_destroy(instance)
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.prefetch_related('product_set').all()
-    serializer_class = CollectionSerializer
-
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
 
     def perform_destroy(self, instance):
