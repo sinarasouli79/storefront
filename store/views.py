b48c1e901ec15ecd098edb2a04ecc6b06dda3cff
sinarasouli79 <sinarasouli79@gmail.com>
2023-04-27 18:44:17 +0330

    generic views

--- store/views.py
+++ store/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
-                                   ListModelMixin, RetrieveModelMixin,
-                                   UpdateModelMixin)
+from rest_framework.generics import (ListCreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Collection, Product
 from .serializers import CollectionSerializer, ProductSerializer
@@ -12,71 +10,33 @@ from .serializers import CollectionSerializer, ProductSerializer
 # Create your views here.
 
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
 
 
-class ProductDetail(APIView):
-    def get(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
-    def put(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        if product.orderitem_set.exists():
+    def perform_destroy(self, instance):
+        if instance.orderitem_set.exists():
             return Response({'error': 'There are some order items associated with this product that you should delete first.'},
                             status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status.HTTP_204_NO_CONTENT)
+        return super().perform_destroy(instance)
 
 
-class CollectionList(APIView):
-    def get(self, request):
-        queryset = Collection.objects.prefetch_related('product_set').all()
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status.HTTP_201_CREATED)
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.prefetch_related('product_set').all()
+    serializer_class = CollectionSerializer
 
 
-class CollectionDetail(APIView):
-    def get(self, requset, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
 
-    def put(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        serializer = CollectionSerializer(collection, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.product_set.exists():
+    def perform_destroy(self, instance):
+        if instance.product_set.exists():
             return Response({'error': "There are some product associated with this collection that you should delete first."},
                             status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status.HTTP_204_NO_CONTENT)
+        return super().perform_destroy(instance)
