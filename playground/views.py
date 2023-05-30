35cda1fab69f7ae14a698220b6d58df86b583088
sinarasouli79 <sinarasouli79@gmail.com>
2023-05-30 18:22:09 +0330

    using low level cache api

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,12 @@
 import requests
+from django.core.cache import cache
 from django.shortcuts import render
 
 
 def say_hello(request):
-    requests.get('https://httpbin.org/delay/2')
-    return render(request, 'playground/hello.html', {'name': 'Sina'})
+    key = 'httpbin'
+    if cache.get(key) is None:
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        cache.set(key, data)
+    return render(request, 'playground/hello.html', {'name': cache.get(key)})
